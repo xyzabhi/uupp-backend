9807d651afb52ad0ab593b6fad9a851023d1e6b3
xyzabhi <abhinavkumar8083@gmail.com>
2025-02-20 20:56:20 +0530

    build: auth code completed basic auth added username+paswword

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"uupp-backend/database"
 	"uupp-backend/handlers"
+	"uupp-backend/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,6 +15,9 @@ func main() {
 
 	app.Post("/register", handlers.RegisterUser)
 	app.Post("/login", handlers.LoginUser)
+	app.Get("/protected", middleware.AuthMiddleware, func(c *fiber.Ctx) error {
+		return c.SendString("This is a protected route")
+	})
 	fmt.Println("Server is running on port 3005")
 	app.Listen(":3005")
 }
